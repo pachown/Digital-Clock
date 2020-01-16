9f41d5a380bca9dc9b3e641e8adb509c0d890df4
Thomas R McNutt <pachown@gmail.com>
2020-01-16 13:55:51 -0700

    Updated Clock & improved code
    Clock is now spaced properly, takes different inputs for each section instead of generic new Date(). AM/PM now appears and runs properly.

--- dom.js
+++ dom.js
@@ -1,35 +1,106 @@
 //Digital clock app JavaScript
 
-//Working but doesn't update itself each second
+//var updateClock = setInterval(updatedClock, 1000);
+var updateYear = setInterval(updatedYear, 1000);
+var updateMonth = setInterval(updatedMonth, 1000);
+var updateDay = setInterval(updatedDay, 1000);
+var updateHoursMinutesSeconds = setInterval(updatedHoursMinutesSeconds, 1000);
 
-//getting date & time
+//This function works but doesn't give specific information to specific IDs, so it is scrapped for now
+// function updatedClock() {
+//     var today = new Date();
+//     document.getElementById('clock').innerHTML = today.toString();
+// }
 
-//var today = new Date();
-//var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-//var dateTime = date+' '+time;
-//document.getElementById('clock').innerHTML = dateTime.toString();
-
-
-var updateEverySecond = setInterval(updatedClock, 1000);
-
-function updatedClock() {
+function updatedYear() {
     var today = new Date();
-    document.getElementById('clock').innerHTML = today.toString();
+    var year = today.getFullYear();
+    document.getElementById('year').innerHTML = year.toString();
 }
 
-////This is finally updating itself! working!
+function updatedMonth() {
+    var today = new Date();
+    var monthNumber = today.getMonth();
+    
+    switch (monthNumber) {
+        case 0:
+            month='January'
+            break;
+        case 1:
+            month='February'
+            break;
+        case 2:
+            month='March'
+            break;
+        case 3: 
+            month='April'
+            break;
+        case 4:
+            month='May'
+            break;
+        case 5:
+            month='June'
+            break;
+        case 6:
+            month='July'
+            break;
+        case 7:
+            month='August'
+            break;
+        case 8:
+            month='September'
+            break;
+        case 9:
+            month='October'
+            break;
+        case 10:
+            month='November'
+            break;
+        case 11:
+            month='December'
+            break;
+    }
+    month=month
+    document.getElementById('month').innerHTML = month.toString();
+}
 
-//To-Do - CSS styling, 
-//      - putting month, day, year, hour, minute, second each in their own place
+function updatedDay() {
+    var today = new Date();
+    var day = today.getDate();
+    document.getElementById('date').innerHTML = day.toString();
+}
 
-//button for military time vs AM PM
+function updatedHours() {
+    var today = new Date();
+    var hours = today.getHours();
+    if (hour <= 12) {
+        hours = hours+'AM'
+    } else {
+        hours = hours-12+'PM'
+    }
+
+    document.getElementById('hour').innerHTML = hours.toString();
+}
+
+function updatedHoursMinutesSeconds () {
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+        if (hours <= 12) {
+            seconds = seconds+'AM';
+        } else {
+            (seconds = seconds+'PM') && (hours = hours-12)
+        }
+    var hoursMinutesSeconds = hours +':'+ minutes+':'+ seconds;
+    document.getElementById('time').innerHTML = hoursMinutesSeconds.toString();
+}
+
+
+//      - get button for military time vs AM PM time to work
 
 //This listens for the button click and then activates the function buttonClick
 var button = document.getElementById('button').addEventListener('click', buttonClick);
 
 //this function is activated when the button is pressed. Right now it just logs something worthless in the console for testing purposes
-//function buttonClick(){
-//    console.log('Button clicked');
-//}
 
